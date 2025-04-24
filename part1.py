72733dce979710d5e3194d14cac90683ea7d26d7
Hadassah1999 <Hadassah.rosenfeld@gmail.com>
2025-04-24 15:15:28 +0300

    commit3

--- part1.py
+++ part1.py
@@ -1,13 +1,43 @@
 import tkinter as tk
 import random
+import time
 
 
 GRID_SIZE = 100
-
-
 PROB_ALIVE = 0.5
+gen = 1
 
-def change_block(grid, i, j): 
+def play_to_end(grid, canvas, cell_size):
+    global gen
+    for i in range(gen, 251):
+        next_gen(grid, canvas, cell_size)
+        canvas.update()
+        time.sleep(0.5)
+
+
+def next_gen(grid, canvas, cell_size):
+    global gen
+    if (gen % 2 == 1):
+        blue_step_no_wrap(grid)
+
+    else:
+        red_step_no_wrap(grid)
+    gen += 1
+    draw_grid(canvas, grid, cell_size)
+
+def back_gen(grid, canvas, cell_size):
+    global gen
+    gen -= 1
+    if (gen % 2 == 0):
+        blue_step_no_wrap(grid)
+
+    else:
+        red_step_no_wrap(grid)
+    draw_grid(canvas, grid, cell_size)
+   
+
+
+def change_block_f(grid, i, j): 
     b1 = grid[i][j]
     b2 = grid[i + 1][j]
     b3 = grid[i][j + 1]
@@ -22,6 +52,12 @@ def change_block(grid, i, j):
         grid[i + 1][j] = b3
         grid[i][j + 1] = b2
         grid[i + 1][j + 1] = b1
+    if(black != 2):
+        b1 = 1 - b1
+        b2 = 1 - b2
+        b3 = 1- b3
+        b4 = 1 - b4
+
 
 
 
@@ -30,7 +66,7 @@ def red_step_no_wrap(grid):
     ##initialize red
     for i in range(1, len(grid), 2):
         for j in range(1, len(grid), 2):
-            change_block(grid, i, j)
+            change_block_f(grid, i, j)
            
 
 
@@ -40,7 +76,7 @@ def blue_step_no_wrap(grid):
      ##initialize red
     for i in range(0, len(grid), 2):
         for j in range(0, len(grid), 2):
-            change_block(grid, i, j)
+            change_block_f(grid, i, j)
            
 
 
@@ -82,16 +118,16 @@ def main():
     toolbar = tk.Frame(root, width=200, bg="lightgray")
     toolbar.pack(side="left", fill="y")
 
-    play_button = tk.Button(toolbar, text="Play", command=None)  
+    play_button = tk.Button(toolbar, text="Play", command=lambda: play_to_end(grid, canvas, cell_size))
     play_button.pack(pady=10)
 
     step_label = tk.Label(toolbar, text=f"Step:", font=("Arial", 12))
     step_label.pack(pady=10)
 
-    next_button = tk.Button(toolbar, text="Next", command=None)  
+    next_button = tk.Button(toolbar, text="Next", command=lambda: next_gen(grid, canvas, cell_size))
     next_button.pack(pady=10)
 
-    back_button = tk.Button(toolbar, text="Back", command=None)  
+    back_button = tk.Button(toolbar, text="Back", command=lambda: back_gen(grid, canvas, cell_size))
     back_button.pack(pady=10)
 
 
